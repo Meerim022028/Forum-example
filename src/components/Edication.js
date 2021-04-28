a632419182880c50c7686e40058eab8df01d4059
Meerim Toktorova <meritoktorova@mail.com>
2021-04-28 16:38:08 +0600

    added new component

---
+++ src/components/Edication.js
@@ -0,0 +1,15 @@
+const Edication = () => {
+    return(
+        <div>
+           <label>Edication</label><br/>
+
+   <input type="radio" id="male" name="gender" value="Shool"/> 
+<label for="male">Male</label> <br/>
+<input type="radio" id="female" name="gender" value="University"/>
+<label for="female">Female</label> <br/>
+
+
+        </div>
+    );
+}
+export default Edication;

0bcd2bff92bde0cf29a0fe36273fac09215e2e61
Meerim Toktorova <meritoktorova@mail.com>
2021-04-28 16:39:07 +0600

    added  nw components

---
+++ src/components/FirstName.js
@@ -0,0 +1,11 @@
+
+
+const FirstName = () => {
+    return(
+        <div>
+<label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"/>
+        </div>
+    );
+}
+export default FirstName;

17a3727fe8f21b7a61f6485c22fc347af30510c8
Meerim Toktorova <meritoktorova@mail.com>
2021-04-28 16:38:29 +0600

    added new components

---
+++ src/components/LastName.js
@@ -0,0 +1,12 @@
+
+
+const LastName = () => {
+    return(
+        <div>
+          <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"/>
+  {/* <input type="submit" value="Submit"/> */}
+        </div>
+    );
+}
+export default LastName;

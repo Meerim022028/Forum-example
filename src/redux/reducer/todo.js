9bf9059741d8d7ceca1aa46d974e5037e2966651
Meerim Toktorova <meritoktorova@mail.com>
2021-05-06 15:23:16 +0600

    sdf

--- src/redux/reducer/todo.js
+++ src/redux/reducer/todo.js
@@ -6,13 +6,18 @@ const todo = (state = initialState, action) => {
       const addState = { ...state };
       const id = Math.random() * 9999999999;
       addState[id.toFixed(0)] = {
-        text: action.text
+        text: action.text,
+        done: false
       };
       return addState;
     case "REMOVE_TODO":
       const removeState = { ...state };
       delete removeState[action.id];
       return removeState;
+    case "TOGGLE_TODO":
+      const toggleState = { ...state };
+      toggleState[action.id].done = !toggleState[action.id].done;
+      return toggleState;
   
     default:
       return state;
